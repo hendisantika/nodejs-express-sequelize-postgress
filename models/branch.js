73cafc0761382269a0fa15e4c2cdadb74035e807
Hendi Santika <hendisantika@gmail.com>
2018-11-11 08:48:45 +0700

    #14 - add an association with `Branch` model inside `associate` function.

---
+++ models/branch.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Branch = sequelize.define('Branch', {
+        company_id: DataTypes.INTEGER,
+        branch_name: DataTypes.STRING,
+        branch_address: DataTypes.STRING,
+        branch_city: DataTypes.STRING
+    }, {});
+    Branch.associate = function (models) {
+        // associations can be defined here
+    };
+    return Branch;
+};
