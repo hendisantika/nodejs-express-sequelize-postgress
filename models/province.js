ce440cc9ad70cbb566d3c9d243b1310679e8e1ae
Hendi Santika <hendisantika@gmail.com>
2018-11-11 08:50:22 +0700

    #16 - add an association with `Province` model inside `City` function.

---
+++ models/province.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Province = sequelize.define('Province', {
+        prov_code: DataTypes.STRING,
+        prov_name: DataTypes.STRING
+    }, {});
+    Province.associate = function (models) {
+        // associations can be defined here
+    };
+    return Province;
+};
+
+Province.associate = function (models) {
+    Province.hasMany(models.City, {
+        foreignKey: 'prov_code',
+        sourceKey: 'prov_code'
+    });
+};
